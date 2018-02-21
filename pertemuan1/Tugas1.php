b440c6d35b605ec78509b3d39630fec40716a764
ju4nda1 <35438710+ju4nda1@users.noreply.github.com>
2018-02-21 19:43:04 +0700

    Add files via upload

---
+++ pertemuan1/Tugas1.php
@@ -0,0 +1,20 @@
+<?php
+// NAMA : M. NAZIL ABDUL GHOFIR
+// KELAS : 3D
+// NIM : 16090099
+
+
+echo " <marquee><h1><center> Tugas Pemprograman Web 1 </marquee></h1></center>";
+
+
+$nim = "16090098";
+$nama = 'M. Nazil Abdul Ghofir';
+$kelas = '3D';
+$jurusan = 'D4 - Teknik Informatika';
+
+echo "NIM : ".$nim. "<br>";
+echo "NAMA : $nama <br>";
+echo "KELAS : $kelas";
+echo "JURUSAN : $jurusan";
+
+?>
---
+++ pertemuan1/perator.php
@@ -0,0 +1,27 @@
+<?php
+
+$panjang = 4;
+$lebar = 2;
+
+$luas = $panjang * $lebar;
+echo $luas; echo "<br>";
+echo $panjang + $lebar; echo "<br>";
+echo $panjang - $lebar; echo "<br>";
+echo $panjang / $lebar; echo "<br>";
+
+$panjang *= $lebar; //panjang = panjang * $lebar
+echo "panjang : $panjang<br><br>";
+
+$a = 6;
+$b = "6";
+
+var_dump($a); echo "<br>";
+var_dump($b); echo "<br>";
+
+var_dump($a == $b); echo "<br>";
+var_dump($a === $b);
+
+$result = ++$a ; //
+echo $result; 
+
+?>
