239ffbf123ec2d2ec920ce312b875714d7b741ec
ju4nda1 <35438710+ju4nda1@users.noreply.github.com>
2018-02-27 17:13:00 +0700

    Update Pertemuan Kedua

---
+++ pertemuan2/Proses.php
@@ -0,0 +1,7 @@
+<h2><center>
+	Selamat Datang, <?php echo $_POST["nama"] ?>
+</h2></center>
+<br>
+<hr>
+Email Anda [<?php echo $_POST["email"] ?>]
+telah terekam
---
+++ pertemuan2/larik-modif.php
@@ -0,0 +1,12 @@
+<?php
+$data = array(
+	"nama" => "Nazil",
+	"usia" => 20,
+	"jomblo" => false
+);
+
+echo "NAMA : ". $data["nama"]."<br>";
+echo "USIA : ". $data["usia"]."<br>";
+echo "JOMBLO ? ". $data["jomblo"]."<br>";
+
+?>
